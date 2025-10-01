9ccd8f3571f5e7d6873b50404b723c90a9265e3e
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-01 12:44:45 +0900

    create job module, service and controller

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
-  imports: [],
+  imports: [JobsModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
