7762c53f277d6d2b89435298c79aa03a2d42278f
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-01 12:45:27 +0900

    write jobs service

--- apps/backend/src/jobs/jobs.service.ts
+++ apps/backend/src/jobs/jobs.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient, Job } from '@prisma/client';
 
 @Injectable()
-export class JobsService {}
+export class JobsService {
+  private prisma = new PrismaClient();
+
+  async createJob(title: string, description: string): Promise<Job> {
+    return this.prisma.job.create({ data: { title, description } });
+  }
+
+  async getJobs(): Promise<Job[]> {
+    return this.prisma.job.findMany();
+  }
+}
