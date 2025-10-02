ac618eb154cb3dcbca4bbc9b3a9e4ecdf84283b0
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-02 12:01:34 +0900

    add empty company module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { JobsModule } from './jobs/jobs.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { JobSeekerModule } from './job-seeker/job-seeker.module';
+import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [JobsModule, AuthModule, UsersModule, JobSeekerModule],
+  imports: [JobsModule, AuthModule, UsersModule, JobSeekerModule, CompanyModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
