4b615eaa02eb2aa1e14708c906758ea55a76d210
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-02 11:58:28 +0900

    add empty job-seeker module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaService } from './prisma.service';
 import { JobsModule } from './jobs/jobs.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { JobSeekerModule } from './job-seeker/job-seeker.module';
 
 @Module({
-  imports: [JobsModule, AuthModule, UsersModule],
+  imports: [JobsModule, AuthModule, UsersModule, JobSeekerModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
