682f6c705e235462261aedc8422ae233f16a8e8e
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-02 17:05:28 +0900

    enable validationpipe globallu

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
   const frontendUrl = process.env.FRONTEND_URL ?? 'http://localhost:3000';
