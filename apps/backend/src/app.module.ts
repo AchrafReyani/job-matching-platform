0ceefb4da8a403862cc9b78c6bc6c1babd984f57
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-02 11:56:35 +0900

    add empty users module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { JobsModule } from './jobs/jobs.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [JobsModule, AuthModule],
+  imports: [JobsModule, AuthModule, UsersModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
