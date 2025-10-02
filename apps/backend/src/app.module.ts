4087375dc03a9270d2a17c832430448a933b32af
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-02 11:53:10 +0900

    add empty auth module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { JobsModule } from './jobs/jobs.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [JobsModule],
+  imports: [JobsModule, AuthModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
