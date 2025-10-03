d6881ffa5a699e1bc5b11a3ed8beb044e3a06561
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-03 15:36:14 +0900

    update auth controller to define endpoints

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register/job-seeker')
+  registerJobSeeker(@Body() dto: any) {
+    return this.authService.registerJobSeeker(dto);
+  }
+
+  @Post('register/company')
+  registerCompany(@Body() dto: any) {
+    return this.authService.registerCompany(dto);
+  }
+
+  @Post('login')
+  login(@Body() dto: { email: string; password: string }) {
+    return this.authService.login(dto.email, dto.password);
+  }
+}
