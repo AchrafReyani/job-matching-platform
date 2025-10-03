3f6445edee28f176641ffdd370392b563100587a
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-03 17:24:03 +0900

    update register job seeker dto to be more robust

--- apps/backend/src/auth/dto/register-jobseeker.dto.ts
+++ apps/backend/src/auth/dto/register-jobseeker.dto.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
 export class RegisterJobSeekerDto {
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
+
+  @IsString()
   fullName: string;
+
+  @IsOptional()
+  @IsString()
   portfolioUrl?: string;
+
+  @IsOptional()
+  @IsString()
   experienceSummary?: string;
 }
