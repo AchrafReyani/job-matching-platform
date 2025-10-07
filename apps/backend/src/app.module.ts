6196720903ae53fc8bd255b22d711ac39b087558
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-07 16:04:04 +0900

    remove code related to initial testing

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { JobsModule } from './jobs/jobs.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { JobSeekerModule } from './job-seeker/job-seeker.module';
 import { CompanyModule } from './company/company.module';
 
 @Module({
-  imports: [JobsModule, AuthModule, UsersModule, JobSeekerModule, CompanyModule],
+  imports: [ AuthModule, UsersModule, JobSeekerModule, CompanyModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
