d35ac254a320c0b87b2206a8d5fa0d6d9739b863
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-07 13:57:42 +0900

    fix eslint error for company regsiter page

--- apps/frontend/app/register/company/page.tsx
+++ apps/frontend/app/register/company/page.tsx
@@ -33,14 +33,19 @@ export default function RegisterCompanyPage() {
         description,
       });
 
-      // ✅ Immediately login after registration
+      // Immediately login after registration
       const { access_token } = await login(email, password);
       saveToken(access_token);
 
       router.push('/profile');
-    } catch (err: any) {
+    } catch (err: unknown) {
       console.error(err);
-      setError('Failed to register. Please check your input.');
+
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Failed to register. Please check your input.');
+      }
     } finally {
       setLoading(false);
     }
