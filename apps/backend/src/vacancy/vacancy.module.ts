e9a4c260b8ead01fdf796fc232de206b0920e768
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-08 18:25:42 +0900

    update vacancy module

--- apps/backend/src/vacancy/vacancy.module.ts
+++ apps/backend/src/vacancy/vacancy.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
-import { VacancyController } from './vacancy.controller';
 import { VacancyService } from './vacancy.service';
+import { VacancyController } from './vacancy.controller';
+import { PrismaModule } from '../prisma/prisma.module'; // assumes you have a PrismaModule
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [PrismaModule, JwtModule, AuthModule],
   controllers: [VacancyController],
-  providers: [VacancyService]
+  providers: [VacancyService],
 })
 export class VacancyModule {}
