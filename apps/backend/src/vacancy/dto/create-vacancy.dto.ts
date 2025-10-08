0cd67e1fbf068647b191a92c0a49f98265b503ab
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-08 18:48:58 +0900

    add separated dto files

---
+++ apps/backend/src/vacancy/dto/create-vacancy.dto.ts
@@ -0,0 +1,6 @@
+export class CreateVacancyDto {
+  title: string;
+  role: string;
+  jobDescription: string;
+  salaryRange?: string;
+}
---
+++ apps/backend/src/vacancy/dto/update-vacancy.dto.ts
@@ -0,0 +1,6 @@
+export class UpdateVacancyDto {
+  title?: string;
+  role?: string;
+  jobDescription?: string;
+  salaryRange?: string;
+}
