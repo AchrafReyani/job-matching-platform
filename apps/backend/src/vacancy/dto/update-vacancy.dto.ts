27fcd78167119be2387372ff3bfb110eebb4a14c
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-08 23:49:16 +0900

    add fix for update dto

--- apps/backend/src/vacancy/dto/update-vacancy.dto.ts
+++ apps/backend/src/vacancy/dto/update-vacancy.dto.ts
@@ -1,6 +1,19 @@
+import { IsOptional, IsString } from 'class-validator';
+
 export class UpdateVacancyDto {
+  @IsOptional()
+  @IsString()
   title?: string;
+
+  @IsOptional()
+  @IsString()
   role?: string;
+
+  @IsOptional()
+  @IsString()
   jobDescription?: string;
+
+  @IsOptional()
+  @IsString()
   salaryRange?: string;
 }
