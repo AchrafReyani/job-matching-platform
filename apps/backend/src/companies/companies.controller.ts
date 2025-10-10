ab9d3535e3617e18afbde069ba726fe93f3336da
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-10 12:57:04 +0900

    implement companies controller

--- apps/backend/src/companies/companies.controller.ts
+++ apps/backend/src/companies/companies.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { CompaniesService } from './companies.service';
 
 @Controller('companies')
-export class CompaniesController {}
+export class CompaniesController {
+  constructor(private readonly companiesService: CompaniesService) {}
+
+  // GET /companies/:id
+  @Get(':id')
+  async getCompany(@Param('id', ParseIntPipe) id: number) {
+    return this.companiesService.getCompanyById(id);
+  }
+
+  // Optional: GET /companies
+  @Get()
+  async getAllCompanies() {
+    return this.companiesService.getAllCompanies();
+  }
+}
