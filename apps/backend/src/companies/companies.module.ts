f493b7020dfc7a3f11bfacf73e6e4473b4aa7a17
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-10 12:57:23 +0900

    implement companies module

--- apps/backend/src/companies/companies.module.ts
+++ apps/backend/src/companies/companies.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { CompaniesController } from './companies.controller';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
-  providers: [CompaniesService],
-  controllers: [CompaniesController]
+  controllers: [CompaniesController],
+  providers: [CompaniesService, PrismaService],
+  exports: [CompaniesService],
 })
 export class CompaniesModule {}
