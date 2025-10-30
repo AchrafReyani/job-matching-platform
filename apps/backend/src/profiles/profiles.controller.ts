f1643c0e0e0ade09c75549328ce74c7128d6809e
Achraf Reyani <achrafreyani99@gmail.com>
2025-10-30 11:57:00 +0900

    add profiles controller

--- apps/backend/src/profiles/profiles.controller.ts
+++ apps/backend/src/profiles/profiles.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { ProfilesService } from './profiles.service';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController {
+  constructor(private profilesService: ProfilesService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getProfile(@Param('id') id: string) {
+    return this.profilesService.getPublicProfile(id);
+  }
+}
