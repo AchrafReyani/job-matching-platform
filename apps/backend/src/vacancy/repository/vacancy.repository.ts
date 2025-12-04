224e3fa2287c060546938edee9c8e818f7f49277
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-05 01:25:44 +0900

    add vacancy interface

---
+++ apps/backend/src/vacancy/repository/vacancy.repository.ts
@@ -0,0 +1,11 @@
+import { CreateVacancyDto } from "../dto/create-vacancy.dto";
+import { UpdateVacancyDto } from "../dto/update-vacancy.dto";
+
+export interface VacancyRepository {
+  create(companyId: number, data: CreateVacancyDto): Promise<any>;
+  update(id: number, companyId: number, data: UpdateVacancyDto): Promise<any>;
+  delete(id: number, companyId: number): Promise<any>;
+  findAll(): Promise<any[]>;
+  findById(id: number): Promise<any | null>;
+  findByCompany(companyId: number): Promise<any[]>;
+}
