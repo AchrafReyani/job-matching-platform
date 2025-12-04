860c9c654a0bb94ce4ae804020b7b89e8a7cab95
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-05 02:15:08 +0900

    update the update vacancy usecase

--- apps/backend/src/vacancy/usecase/update-vacancy.usecase.ts
+++ apps/backend/src/vacancy/usecase/update-vacancy.usecase.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
-import type { VacancyRepository } from '../repository/vacancy.repository';
+import { Injectable, Inject } from '@nestjs/common';
+import * as vacancyRepository from '../repository/vacancy.repository';
 import { UpdateVacancyDto } from '../dto/update-vacancy.dto';
 
 @Injectable()
 export class UpdateVacancyUseCase {
-  constructor(private readonly vacancyRepository: VacancyRepository) {}
+  constructor(
+    @Inject(vacancyRepository.VACANCY_REPOSITORY)
+    private readonly vacancyRepository: vacancyRepository.VacancyRepository,
+  ) {}
 
   async execute(id: number, companyId: number, data: UpdateVacancyDto) {
     return this.vacancyRepository.update(id, companyId, data);
