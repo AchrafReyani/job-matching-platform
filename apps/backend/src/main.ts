e78316e3b33d47b4430dd5f717e0bd42cf149ead
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-05 03:27:45 +0900

    make sure swagger is only available locally

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,23 +5,27 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-
-  const config = new DocumentBuilder()
-    .setTitle('API Docs')
-    .setDescription('My API description')
-    .setVersion('1.0')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
 
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
   const frontendUrl = process.env.FRONTEND_URL ?? 'http://localhost:3000';
-
   app.enableCors({
     origin: frontendUrl,
   });
+
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('API Docs')
+      .setDescription('My API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+    console.log('Swagger enabled at /api-docs');
+  }
 
   await app.listen(port);
   console.log(`Nest app listening on port ${port}`);
