9010f0209c40ae07f8a7d4557d3bb623c730838a
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-05 02:19:36 +0900

    update get vacancies usecase

--- apps/backend/src/vacancy/usecase/get-vacancies.usecase.ts
+++ apps/backend/src/vacancy/usecase/get-vacancies.usecase.ts
@@ -1,9 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import type { VacancyRepository } from '../repository/vacancy.repository';
+import { Injectable, Inject } from '@nestjs/common';
+import * as vacancyRepository from '../repository/vacancy.repository';
 
 @Injectable()
 export class GetVacanciesUseCase {
-  constructor(private readonly vacancyRepository: VacancyRepository) {}
+  constructor(
+    @Inject(vacancyRepository.VACANCY_REPOSITORY)
+    private readonly vacancyRepository: vacancyRepository.VacancyRepository,
+  ) {}
 
   async execute() {
     return this.vacancyRepository.findAll();
