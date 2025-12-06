74b20aaf2ef3d03dc995ea19a4a750ad2620fba0
Achraf Reyani <achrafreyani99@gmail.com>
2025-12-06 21:50:15 +0900

    delete unused job seeker and company routes

--- apps/backend/src/job-seeker/job-seeker.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { JobSeekerController } from './job-seeker.controller';
-import { JobSeekerService } from './job-seeker.service';
-
-@Module({
-  controllers: [JobSeekerController],
-  providers: [JobSeekerService],
-})
-export class JobSeekerModule {}
